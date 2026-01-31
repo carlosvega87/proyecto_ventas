c3095915952477a653a2d925e218e4366b9170ff
carlosvega87 <cvega@tecnologicoismac.edu.ec>
2026-01-31 18:35:09 -0500

    versionamiento2.1

--- ventas/src/app/app-module.ts
+++ ventas/src/app/app-module.ts
@@ -13,10 +13,12 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
 import{ MatButtonModule} from '@angular/material/button';
 import { MovimientoInventarioComponent } from './components/movimientoinventario/movimientoinventario';
+import { ClienteComponent } from './components/cliente/cliente';
 @NgModule({
   declarations: [
     App,
     MovimientoInventarioComponent,
+  ClienteComponent,
     
   ],
   imports: [
